0f7e0f6bf9f25b76159ea055ec1b1dc167e58418
David <105886661+RomeroRodriguezD@users.noreply.github.com>
2025-02-05 00:54:12 +0100

    Create models.py

---
+++ database/models.py
@@ -0,0 +1,21 @@
+from sqlalchemy import create_engine, Column, Integer,Text
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+class RomanModel(Base):
+    __tablename__ = 'roman_chars'
+    id = Column(Integer, primary_key=True, autoincrement=True)
+    name = Column(Text)
+    year_birth = Column(Text)
+    year_dead = Column(Text)
+    role = Column(Text)
+    curiosities = Column(Text)
+
+# This code below was only used to create the database.
+
+#engine = create_engine('sqlite:///roman.db')
+#Session = sessionmaker(bind=engine)
+#session = Session()
+#Base.metadata.create_all(bind=engine)
